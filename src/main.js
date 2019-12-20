ad3926efb7190564b31b9325dc2128f47e4f9e6f
Langery <1245066197@qq.com>
2019-12-20 09:42:27 +0800

    rewrite

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { version } from '../package.json'
+
 Vue.config.productionTip = false
+Vue.prototype.VERSION = version
+
+Vue.use(router)
 
 new Vue({
   router,
